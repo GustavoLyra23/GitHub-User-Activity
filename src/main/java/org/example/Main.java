2b534f45424a4347977e2522099476caa49eea73
GustavoLyra23 <gustavolyra23@gmail.com>
2024-10-21 19:30:06 -0300

    askUsername method created

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,43 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        askUsername(sc);
 
     }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    private static String askUsername(Scanner sc) {
+        String username = sc.nextLine().trim();
+        String usernamePattern = "^[a-zA-Z0-9_<>]{3,15}$";
+        Pattern pattern = Pattern.compile(usernamePattern);
+        Matcher matcher = pattern.matcher(username);
+        if (matcher.matches()) {
+            System.out.println("Username is valid.");
+        } else {
+            System.out.println("Username is invalid.");
+        }
+        return username;
+    }
+
+
+
+
 }
